4eac5c605537446c21c04701401af44bb7e82677
vimal-pv <vimal.pillai@vofoxsolutions.com>
2021-02-23 13:49:36 +0530

    app.js content changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there. I am batman. Also superman. hmmmm.. href
+          Hi there. I am studying hmmmm.. href
         </p>
         <p>Nive tutorial by the way!!</p>
         <a
